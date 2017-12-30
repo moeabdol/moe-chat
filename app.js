9f6ef7c52e0f1da77f7046e2d2fb893cbb34cc96
moeabdol <mohd.a.saed@gmail.com>
2017-12-30 19:26:44 +0300

    Configure socket.io

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
-const app = express();
+const app     = express();
+const http    = require('http').Server(app);
+const io      = require('socket.io')(http);
 
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
 
-app.listen(3000, err => {
+io.on('connection', socket => {
+  console.log(`connected to ${socket.id}`);
+  // console.log(socket);
+});
+
+http.listen(3000, err => {
   if (err) return console.log(err);
   console.log('Server connected on port 3000');
 });
---
+++ public/chat.js
@@ -0,0 +1,2 @@
+var socket = io();
+socket.connect('http://localhost:3000');
